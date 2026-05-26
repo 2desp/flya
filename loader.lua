efe4e491011d614ca6cf15c0c6cc4d74963f483c
2desp <iliveinsingapure@gmail.com>
2026-05-26 15:01:54 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,3 @@
+if game.gameId == 9348272796 then -- Survive Zombie Arena
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/2desp/flya/refs/heads/main/Survive%20Zombie%20Arena-obfuscated.lua"))()
+end 
