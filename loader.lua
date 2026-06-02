03af24674d81fbcaac8d10457d0ad7e3ef48b7d9
2desp <iliveinsingapure@gmail.com>
2026-06-02 15:02:19 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,7 @@
 if game.gameId == 9348272796 then -- Survive Zombie Arena
   loadstring(game:HttpGet("https://raw.githubusercontent.com/2desp/flya/refs/heads/main/Survive%20Zombie%20Arena-obfuscated.lua"))()
 end 
+if game.gameId == 10111742174 then -- Defend ur base with anime
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/2desp/flya/refs/heads/main/Defend%20ur%20base%20with%20anime-obfuscated.lua"))()
+end 
+
