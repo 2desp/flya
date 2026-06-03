131c10b005d2f54488b147632884f3ace526a87a
2desp <iliveinsingapure@gmail.com>
2026-06-03 16:55:22 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,3 +5,8 @@ if game.gameId == 10111742174 then -- Defend ur base with anime
   loadstring(game:HttpGet("https://raw.githubusercontent.com/2desp/flya/refs/heads/main/Defend%20ur%20base%20with%20anime-obfuscated.lua"))()
 end 
 
+
+
+if game.gameId == 10148434559 then -- Lucky Block Rush
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/2desp/flya/refs/heads/main/Lucky%20Block%20Rush-obfuscated.lua"))()
+end 
